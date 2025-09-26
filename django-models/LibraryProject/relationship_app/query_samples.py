fe2766cc962867a2e730203239da3b9942db4657
George <george.milad0001@gmail.com>
2025-09-26 15:26:56 +0300

    advanced models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 author = Author.objects.get(name="Author Name")
-name = "Library Name"
+library_name = "Library Name"
 
 # Books by author
 books_by_author = Book.objects.filter(author=author)
@@ -9,8 +9,8 @@ for book in books_by_author:
     print(book.title)
 
 # Books in library
-library = Library.objects.get(name=name)
-book_in_library= Library.book_set.all()
+library = Library.objects.get(name=library_name)
+book_in_library= Library.books.all()
 for book in book_in_library:
     print(book.title)
 
