d56fe19af4132ca5ba4c2125392fcc0bfa3ca7fd
George <george.milad0001@gmail.com>
2025-09-28 18:27:11 +0300

    User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 # Create your views here.
