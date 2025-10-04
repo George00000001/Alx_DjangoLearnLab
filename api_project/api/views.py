129dff8d519aa30ca5daca32a1fe6c345c703757
George <george.milad0001@gmail.com>
2025-10-04 16:02:12 +0300

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
-
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -12,3 +10,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -122,3 +123,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+"""
+Authentication & Permissions Setup:
+----------------------------------
+- Token Authentication enabled via 'rest_framework.authtoken'
+- Obtain token at endpoint: /api/token/
+- Default permission: IsAuthenticated
+- Custom permissions can be set in each ViewSet as needed
+"""
